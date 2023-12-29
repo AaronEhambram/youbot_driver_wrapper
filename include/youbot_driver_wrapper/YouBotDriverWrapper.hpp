4d40159f7ccd3d9ab909e9c1a75850195b66903d
AaronEhambram <aaronkumare@googlemail.com>
2023-12-30 00:54:41 +0100

    add the manipulator. impement joint state publisher

--- include/youbot_driver_wrapper/YouBotDriverWrapper.hpp
+++ include/youbot_driver_wrapper/YouBotDriverWrapper.hpp
@@ -1,7 +1,11 @@
 #include "rclcpp/rclcpp.hpp"
 #include "youbot/YouBotBase.hpp"
+#include "youbot/YouBotManipulator.hpp"
 #include "nav_msgs/msg/odometry.hpp"
 #include "geometry_msgs/msg/twist.hpp"
+#include "sensor_msgs/msg/joint_state.hpp"
+#include <tf2_ros/transform_broadcaster.h>
+#include <thread>
 
 using std::placeholders::_1;
 using namespace youbot;
@@ -20,10 +24,21 @@ class YouBotDriverWrapper : public rclcpp::Node
     rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_publisher_;
     rclcpp::TimerBase::SharedPtr timer_;
     void odom_publisher_callback();
-    nav_msgs::msg::Odometry odom_msg_ ; 
+    nav_msgs::msg::Odometry odom_msg_; 
+
+    // tf2 broadcaster
+    std::shared_ptr<tf2_ros::TransformBroadcaster> tfb_;
+
+    // joint publisher
+    std::thread joint_publisher_thread_; 
+    sensor_msgs::msg::JointState joint_state_msg_; 
+    rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr joint_state_publisher_;
+    void publish_joint_state(); 
 
     // YouBot specific
     std::string youbot_configuration_dir = "/home/aaron/workspace/libraries/youbot_driver/config";
     YouBotBase* myYouBotBase = 0;
     bool youBotHasBase = false;
+    YouBotManipulator* myYouBotManipulator = 0; 
+    bool youBotHasArm = false; 
 };
--- src/YouBotDriverWrapper.cpp
+++ src/YouBotDriverWrapper.cpp
@@ -1,5 +1,6 @@
 #include <memory>
 #include "youbot_driver_wrapper/YouBotDriverWrapper.hpp"
+#include <geometry_msgs/msg/transform_stamped.hpp>
 
 using std::placeholders::_1;
 using namespace youbot;
@@ -13,6 +14,9 @@ YouBotDriverWrapper::YouBotDriverWrapper() : Node("youbot_driver_wrapper_node")
   odom_publisher_ = this->create_publisher<nav_msgs::msg::Odometry>("odom", 10);
   timer_ = this->create_wall_timer(10ms, std::bind(&YouBotDriverWrapper::odom_publisher_callback, this));
 
+  // setup tf broadcaster
+  tfb_ = std::make_shared<tf2_ros::TransformBroadcaster>(this);
+
   // create handles for youBot base and manipulator (if available)
   try {
     myYouBotBase = new YouBotBase("youbot-base", youbot_configuration_dir);
@@ -21,6 +25,63 @@ YouBotDriverWrapper::YouBotDriverWrapper() : Node("youbot_driver_wrapper_node")
   } catch (std::exception& e) {
     LOG(warning) << e.what();
     youBotHasBase = false;
+  }
+
+  try {
+		myYouBotManipulator = new YouBotManipulator("youbot-manipulator", youbot_configuration_dir);
+		myYouBotManipulator->doJointCommutation();
+		myYouBotManipulator->calibrateManipulator();
+		myYouBotManipulator->calibrateGripper();
+		youBotHasArm = true;
+	} catch (std::exception& e) {
+		LOG(warning) << e.what();
+		youBotHasArm = false;
+	}
+
+  // start the joint state publishing in a seperate thread
+  joint_state_msg_.name.resize(11);
+  joint_state_msg_.position.resize(11); 
+  // base wheels
+  joint_state_msg_.name[0] = "wheel_joint_fl"; joint_state_msg_.name[1] = "wheel_joint_fr"; joint_state_msg_.name[2] = "wheel_joint_bl";
+  joint_state_msg_.name[3] = "wheel_joint_br";
+  // arm
+  joint_state_msg_.name[4] = "arm_joint_1"; joint_state_msg_.name[5] = "arm_joint_2"; joint_state_msg_.name[6] = "arm_joint_3";
+  joint_state_msg_.name[7] = "arm_joint_4"; joint_state_msg_.name[8] = "arm_joint_5"; 
+  // gripper
+  joint_state_msg_.name[9] = "gripper_finger_joint_l"; joint_state_msg_.name[10] = "gripper_finger_joint_r"; 
+  // create teh message object
+  joint_state_publisher_ = this->create_publisher<sensor_msgs::msg::JointState>("joint_states", 10);
+  // start the seperate thread
+  std::thread publisher_thread(&YouBotDriverWrapper::publish_joint_state, this);
+  joint_publisher_thread_ = std::move(publisher_thread); 
+}
+
+void YouBotDriverWrapper::publish_joint_state()
+{
+  rclcpp::Rate loop_rate(15);
+  while(rclcpp::ok())
+  {
+    std::vector<JointSensedAngle> base_data;
+    myYouBotBase->getJointData(base_data);
+    std::vector<JointSensedAngle> arm_data;
+    myYouBotManipulator->getJointData(arm_data);
+
+    joint_state_msg_.header.stamp = now(); 
+    joint_state_msg_.header.frame_id = "";
+
+    // base wheels
+    joint_state_msg_.position[0] = -base_data[0].angle.value();
+    joint_state_msg_.position[1] = base_data[1].angle.value();
+    joint_state_msg_.position[2] = -base_data[2].angle.value();
+    joint_state_msg_.position[3] = base_data[3].angle.value();
+    
+    // arm joints
+    for(int i = 0; i < arm_data.size(); ++i)
+    {
+      joint_state_msg_.position[i+4] = arm_data[i].angle.value();
+    }
+    joint_state_publisher_->publish(joint_state_msg_);
+    loop_rate.sleep();
   }
 }
 
@@ -40,7 +101,7 @@ void YouBotDriverWrapper::odom_publisher_callback()
   myYouBotBase->getBasePosition(longitudinalPosition,transversalPosition,orientation);
   odom_msg_.header.frame_id = "odom";
   odom_msg_.header.stamp = now(); 
-  odom_msg_.child_frame_id = "youbot_base";
+  odom_msg_.child_frame_id = "base_footprint";
 
   // pose
   odom_msg_.pose.pose.position.x = longitudinalPosition.value();
@@ -62,6 +123,18 @@ void YouBotDriverWrapper::odom_publisher_callback()
   odom_msg_.twist.twist.angular.x = 0;
   odom_msg_.twist.twist.angular.y = 0;
   odom_msg_.twist.twist.angular.z = angular_velocity.value();
+
+  // create broadcaster for tf-tree
+  geometry_msgs::msg::TransformStamped tfs_;
+  tfs_.header = odom_msg_.header;
+  tfs_.header.stamp = odom_msg_.header.stamp;
+  tfs_.header.frame_id = odom_msg_.header.frame_id;
+  tfs_.child_frame_id = odom_msg_.child_frame_id;
+  tfs_.transform.translation.x = odom_msg_.pose.pose.position.x;
+  tfs_.transform.translation.y = odom_msg_.pose.pose.position.y;
+  tfs_.transform.translation.z = odom_msg_.pose.pose.position.z;
+  tfs_.transform.rotation = odom_msg_.pose.pose.orientation;
+  tfb_->sendTransform(tfs_);
 
   odom_publisher_->publish(odom_msg_); 
 }
